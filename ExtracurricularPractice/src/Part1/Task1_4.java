468d05b4c469794737c7422ea14c9ea8f2e704ef
Jason Song <wolfogre@outlook.com>
2016-01-04 13:17:55 +0800

    Copied Task1_3 to Task1_4

--- ExtracurricularPractice/src/Part1/Task1_4.java
+++ ExtracurricularPractice/src/Part1/Task1_4.java
@@ -4,4 +4,24 @@ package Part1;
  * Created by Jason Song(wolfogre@outlook.com) on 01/02/2016.
  */
 public class Task1_4 {
+	public static void main(String args[]){
+		System.out.println("13121602-宋建鑫:");
+		for(int i = 4; i <= 100; i += 2){
+			for(int j = 2; j < i; ++j){
+				if(isPrime(j) && isPrime(i - j)){
+					System.out.println(i + " = " + j + " + " + (i - j));
+					break;
+				}
+			}
+		}
+	}
+	static boolean isPrime(int n){
+		if(n < 2)
+			return false;
+		for(int i = 2; i < n / 2; ++i){
+			if(n % i == 0)
+				return false;
+		}
+		return true;
+	}
 }
