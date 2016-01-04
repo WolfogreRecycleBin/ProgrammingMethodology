1c9bc24d40652e75630c2eddf12f5f72cd72d038
Jason Song <wolfogre@outlook.com>
2016-01-04 13:36:34 +0800

    Finished Part2

--- ExtracurricularPractice/src/Part2/Practice2_1.java
+++ ExtracurricularPractice/src/Part2/Practice2_1.java
@@ -1,7 +1,23 @@
 package Part2;
 
+import java.util.Random;
+
 /**
  * Created by Jason Song(wolfogre@outlook.com) on 01/02/2016.
  */
 public class Practice2_1 {
+	public static void main(String args[]){
+		Random random = new Random();
+		System.out.println("13121602-宋建鑫:");
+		int x = random.nextInt();
+		int y = random.nextInt();
+		int z;
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		if(x > y)
+			z = x;
+		else
+			z = y;
+		System.out.println("z = " + z);
+	}
 }
--- ExtracurricularPractice/src/Part2/Task2_1.java
+++ ExtracurricularPractice/src/Part2/Task2_1.java
@@ -4,4 +4,8 @@ package Part2;
  * Created by Jason Song(wolfogre@outlook.com) on 01/02/2016.
  */
 public class Task2_1 {
+	public static void main(String args[]){
+		Practice2_1.main(null);
+		//WTF...
+	}
 }
